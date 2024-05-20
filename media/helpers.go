47a2c2a33594b275a1c4b915f0d78644ca1fc2a8
Siddhant Madhur <siddhant.madhur@gmail.com>
2024-05-19 18:13:29 -0700

    Added function to generate content for .m3u8 file

--- media/helpers.go
+++ media/helpers.go
@@ -11,11 +11,39 @@ func GetLengthOfFile(path string) (float64, error) {
 	probe := exec.Command("ffprobe", "-i", path, "-show_entries", "format=duration", "-v", "quiet", "-of", `csv=p=0`)
 	output, err := probe.Output()
 	if err != nil {
-		fmt.Printf("[ERROR]:\n")
-		fmt.Print(string(output))
-		fmt.Printf("\n[END OF ERROR]\n")
 		return 0, err
 	}
 	size, err := strconv.ParseFloat(strings.ReplaceAll(string(output), "\n", ""), 64)
 	return size, err
+}
+
+// Returns content of m3u8 file as a string or error
+func CreatePlaylistHLSFile(path string) (string, error) {
+	size, err := GetLengthOfFile(path)
+	counter := size
+	if err != nil {
+		return "", err
+	}
+	content := "#EXTM3U\n"
+	content += "#EXT-X-VERSION:3\n"
+	content += "#EXT-X-TARGETDURATION:2\n"
+	content += "#EXT-X-MEDIA-SEQUENCE:0\n"
+
+	idx := 0
+	for counter > 0.0 {
+		newTime := 0.0
+		if counter >= 2.0 {
+			newTime = 2.0
+		} else {
+			newTime = counter
+		}
+		content += fmt.Sprintf("#EXTINF:%.6f,\n", newTime)
+		content += fmt.Sprintf("stream%d.ts\n", idx)
+		counter -= newTime
+		idx += 1
+	}
+
+	content += "#EXT-X-ENDLIST"
+	return content, nil
+
 }
--- media/helpers_test.go
+++ media/helpers_test.go
@@ -2,12 +2,15 @@ package media
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"strings"
 	"testing"
 )
 
 func TestLengthOfFile(t *testing.T) {
 	homedir, _ := os.UserHomeDir()
+	// this is where i have my test footage located
 	file := homedir + "/github/ffmpeg-trial/tmnt.mp4"
 	fmt.Printf("[READING] %s\n", file)
 	size, err := GetLengthOfFile(file)
@@ -16,4 +19,23 @@ func TestLengthOfFile(t *testing.T) {
 		t.FailNow()
 	}
 	fmt.Printf("[SUCCESS]: %f\n", size)
+}
+
+func TestPlaylistCreation(t *testing.T) {
+	homedir, _ := os.UserHomeDir()
+	// this is where i have my test footage located
+	file := homedir + "/github/ffmpeg-trial/tmnt.mp4"
+
+	// lol = (length of file * 0.5) + 4
+	content, err := CreatePlaylistHLSFile(file)
+	if err != nil {
+		fmt.Printf("[ERROR]: %s\n", err.Error())
+		t.FailNow()
+	}
+	size := len(strings.Split(content, "\n"))
+	lengthOfFile, _ := GetLengthOfFile(file)
+	if size != int(math.Ceil(lengthOfFile))+5 {
+		fmt.Printf("[ERROR]: Expected length: %d, got: %d\n", int(math.Ceil(lengthOfFile))+5, size)
+		t.FailNow()
+	}
 }
