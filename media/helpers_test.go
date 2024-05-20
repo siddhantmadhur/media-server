1484101b1ff83ac13ec0a65422762ac5f05ba4be
Siddhant Madhur <siddhant.madhur@gmail.com>
2024-05-19 18:16:19 -0700

    Removed print statements from tests

--- media/helpers_test.go
+++ media/helpers_test.go
@@ -12,13 +12,11 @@ func TestLengthOfFile(t *testing.T) {
 	homedir, _ := os.UserHomeDir()
 	// this is where i have my test footage located
 	file := homedir + "/github/ffmpeg-trial/tmnt.mp4"
-	fmt.Printf("[READING] %s\n", file)
-	size, err := GetLengthOfFile(file)
+	_, err := GetLengthOfFile(file)
 	if err != nil {
 		fmt.Printf("[ERROR]: %s\n", err.Error())
 		t.FailNow()
 	}
-	fmt.Printf("[SUCCESS]: %f\n", size)
 }
 
 func TestPlaylistCreation(t *testing.T) {
