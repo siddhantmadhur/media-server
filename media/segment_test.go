f28b60e3b5a532321f2aafb6b1f179b201565114
Siddhant Madhur <siddhant.madhur@gmail.com>
2024-06-05 13:42:00 -0700

    Improved testing for segment.go

--- media/segment_test.go
+++ media/segment_test.go
@@ -1,6 +1,7 @@
 package media
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -9,13 +10,16 @@ func TestNewSegment(t *testing.T) {
 	initialSegment.StartSegment = 0
 	initialSegment.EndSegment = 100
 
-	initialSegment.AddNewSegment(25)
+	initialSegment.AddNewSegment(30)
+	initialSegment.AddNewSegment(13)
 
 	cur := &initialSegment
 	idx := 0
-	expected := []int{0, 24, 25, 100}
+	expected := []int64{0, 12, 13, 29, 30, 100}
 	for cur != nil {
-		if cur.StartSegment != int64(expected[idx]) && cur.EndSegment != int64(expected[idx+1]) {
+		if cur.StartSegment != expected[idx] && cur.EndSegment != int64(expected[idx+1]) {
+			fmt.Printf("Expected: %d, got: %d\n", expected[idx], cur.StartSegment)
+			fmt.Printf("Expected: %d, got: %d\n", expected[idx+1], cur.EndSegment)
 			t.FailNow()
 		}
 		idx += 2
